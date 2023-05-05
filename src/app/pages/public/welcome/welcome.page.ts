187c10761e084a75cc8be84483cb1aa2e8044fea
Ajay Kumar <ajay.ce.kumar@avanade.com>
2023-05-05 14:33:12 +0200

    download pwa

--- src/app/pages/public/welcome/welcome.page.ts
+++ src/app/pages/public/welcome/welcome.page.ts
@@ -1,4 +1,11 @@
-import { AfterContentChecked, ChangeDetectorRef, Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import {
+  AfterContentChecked,
+  ChangeDetectorRef,
+  Component,
+  HostListener,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { SwiperComponent } from 'swiper/angular';
 import SwiperCore, { SwiperOptions, Pagination } from 'swiper';
 SwiperCore.use([Pagination]);
@@ -12,6 +19,17 @@ import { Router } from '@angular/router';
   encapsulation: ViewEncapsulation.None,
 })
 export class WelcomePage implements AfterContentChecked {
+  deferredPrompt: any;
+  showButton = false;
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onbeforeinstallprompt(e) {
+    console.log(e);
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    this.deferredPrompt = e;
+    this.showButton = true;
+  }
 
   language: string = '';
   last_slide: boolean = false;
@@ -23,16 +41,28 @@ export class WelcomePage implements AfterContentChecked {
     slidesPerView: 1,
     spaceBetween: 50,
     pagination: { clickable: false },
-    allowTouchMove: false // set true to allow swiping
+    allowTouchMove: false, // set true to allow swiping
+  };
+
+  constructor(private router: Router, private ref: ChangeDetectorRef) {}
+
+  addToHomeScreen() {
+    // hide our user interface that shows our A2HS button
+    this.showButton = false;
+    // Show the prompt
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      this.deferredPrompt = null;
+    });
   }
 
-  constructor(
-    private router: Router,
-    private ref: ChangeDetectorRef
-  ) { }
-
   ngAfterContentChecked(): void {
-
     if (this.swiper) {
       this.swiper.updateSwiper({});
     }
@@ -55,9 +85,7 @@ export class WelcomePage implements AfterContentChecked {
 
   // Go to main content
   async getStarted() {
-
     // Navigate to /home
     this.router.navigateByUrl('/signin');
   }
-
 }
