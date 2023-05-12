ae14fcf5a5352d6fbc441283aedc2465b4b9758a
AK <akshaykumar71297@gmail.com>
2023-05-12 12:30:07 -0600

    fix vercel

--- src/app/pages/secure/home/home.module.ts
+++ src/app/pages/secure/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -14,8 +14,9 @@ import {MapComponent} from "./map/map.component";
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [HomePage, MapComponent]
 })
 export class HomePageModule {}
